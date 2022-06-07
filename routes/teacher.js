0def55331b9644046608099eaf76374071977605
Vedanti Sharma <vedanti28sharma@gmail.com>
2022-06-07 06:32:45 +0530

    7 june

--- routes/teacher.js
+++ routes/teacher.js
@@ -1,11 +1,57 @@
-var express = require('express');
+// var express = require('express');
+const res = require('express/lib/response');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
- console.log(req)
- res.json({name: "vedantis"})
-    // res.send('respond with a resource');
+/* GET home page. */
+// router.get('/data', function(req, res, next) {
+//   console.log(req);//print the req
+//   res.json({name:"SHivam beta"}); //send the response to the server basically a (Get request) 
+//   // res.render('index', { title: 'Express' });
+// });
+
+// router.get('/:da', function(req, res, next) {
+//     // console.log(res.params.da);//print the req
+//     res.json({name:req.params.da});//Params used to take variable (teacher/hahah() 
+//     // res.render('index', { title: 'Express' });
+// });
+
+// router.get('/:da/:ta', function(req, res, next) {//intialisinf two var 
+//     // console.log(res.params.da);//
+//     res.json({name:req.params.da+"is and " +req.params.ta+ "is from kcc"});//sending res and print  
+//     // res.render('index', { title: 'Express' });
+//   });
+
+// router.get('/:da/:ta', function(req, res, next){//intialisinf two var 
+//     console.log(res.params.da);//apply params
+//     res.json({name:req.params.da+"is and " +req.params.ta+ "is from kcc"});//sending res and print  
+    // res.render('index', { title: 'Express' });
+//     var x = Number(req.params.da);
+//     var y = Number(req.params.ta);
+//     var z = x+y; //adding
+//     res.json({sum:z});
+  // });
+router.get('/:da', function(req, res,) {
+  // res.json({name:"Saurav"});//send the response to the server basically a (Get request) 
+  res.render('teacher', { title: req.params.da,name1:req.params });
+});
+
+// router.get('/data', function(req, res, next) {
+//   console.log(req.query);//print the req
+//   // res.json({name:"SHivam beta"}); //send the response to the server basically a (Get request) 
+//  res.render('index', { title: 'Express' });
+// });
+
+// router.get('/data', function(req, res, next) {
+//   console.log(req.query);//print the req
+//   // res.json({name:"SHivam beta"}); //send the response to the server basically a (Get request) 
+//  res.render('index', { title: req.query.fname});
+// });
+
+router.post('/data', function(req, res, next) {
+  console.log(req.body);//print the req
+  console.log(req.query.lname);
+  // res.json({name:"SHivam beta"}); //send the response to the server basically a (Get request) 
+ res.render('index', { title: req.body.fname});
 });
 
 module.exports = router;
