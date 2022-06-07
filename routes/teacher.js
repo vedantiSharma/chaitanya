8134ade9b70d8391fa545b10ae3d09907790b4bf
Vedanti Sharma <vedanti28sharma@gmail.com>
2022-06-07 06:36:15 +0530

    7june2022

--- routes/teacher.js
+++ routes/teacher.js
@@ -1,57 +1,36 @@
-// var express = require('express');
-const res = require('express/lib/response');
+var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-// router.get('/data', function(req, res, next) {
-//   console.log(req);//print the req
-//   res.json({name:"SHivam beta"}); //send the response to the server basically a (Get request) 
-//   // res.render('index', { title: 'Express' });
-// });
-
-// router.get('/:da', function(req, res, next) {
-//     // console.log(res.params.da);//print the req
-//     res.json({name:req.params.da});//Params used to take variable (teacher/hahah() 
-//     // res.render('index', { title: 'Express' });
-// });
-
-// router.get('/:da/:ta', function(req, res, next) {//intialisinf two var 
-//     // console.log(res.params.da);//
-//     res.json({name:req.params.da+"is and " +req.params.ta+ "is from kcc"});//sending res and print  
-//     // res.render('index', { title: 'Express' });
-//   });
-
-// router.get('/:da/:ta', function(req, res, next){//intialisinf two var 
-//     console.log(res.params.da);//apply params
-//     res.json({name:req.params.da+"is and " +req.params.ta+ "is from kcc"});//sending res and print  
-    // res.render('index', { title: 'Express' });
-//     var x = Number(req.params.da);
-//     var y = Number(req.params.ta);
-//     var z = x+y; //adding
-//     res.json({sum:z});
-  // });
-router.get('/:da', function(req, res,) {
-  // res.json({name:"Saurav"});//send the response to the server basically a (Get request) 
-  res.render('teacher', { title: req.params.da,name1:req.params });
-});
-
-// router.get('/data', function(req, res, next) {
-//   console.log(req.query);//print the req
-//   // res.json({name:"SHivam beta"}); //send the response to the server basically a (Get request) 
-//  res.render('index', { title: 'Express' });
-// });
-
-// router.get('/data', function(req, res, next) {
-//   console.log(req.query);//print the req
-//   // res.json({name:"SHivam beta"}); //send the response to the server basically a (Get request) 
-//  res.render('index', { title: req.query.fname});
-// });
 
 router.post('/data', function(req, res, next) {
-  console.log(req.body);//print the req
-  console.log(req.query.lname);
-  // res.json({name:"SHivam beta"}); //send the response to the server basically a (Get request) 
- res.render('index', { title: req.body.fname});
+  console.log(req.body)
+  console.log(req.query.lname)
+  // res.json({name:req.query.fname})
+  res.render('index', { title: req.body.fname });
 });
 
-module.exports = router;
+router.get('/data', function(req, res, next) {
+  console.log(req.query)
+  // res.json({name:req.query.fname})
+  res.render('index', { title: req.query.fname });
+});
+
+router.get('/:da-:ta-:ch', function(req, res, next) {
+    console.log(req.params )
+    // res.json({name:req.params.da+","+req.params.ch +" and "+req.params.ta+" are bother and sister"})
+    res.render('teacher', { title: req.params.da,name1:req.params.ta,verb:req.params.ch});
+
+  });
+
+router.get('/:da-:ta', function(req, res, next) {
+    console.log(req.params.da)
+    // res.json({name:req.params.da+" and "+req.params.ta+" are bother and sister"})
+    res.render('teacher', { title: req.params.da,name1:req.params.ta});
+  });
+
+router.get('/:da', function(req, res, next) {
+    console.log(req.params.da)
+    // res.json({name:req.params.da})
+    res.render('teacher', { title: req.params.da});
+  });
