d7aac5e14c28d7ae6983f5a5a6dbd91879e6f227
Vedanti Sharma <vedanti28sharma@gmail.com>
2022-06-11 15:20:52 +0530

    creating url

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+   // res.render('index', { title: 'Express' });
+     router.post('/getLogin', function(req, res, next) {
+         console.log(req.body);//print the req
+         
+        res.render('index', { title: req.body.fname});
+  });
 });
+
 
 module.exports = router;
