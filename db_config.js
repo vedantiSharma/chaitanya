7c65452d0080cbbfe4a4a99b25776cad86fc6ae7
Vedanti Sharma <vedanti28sharma@gmail.com>
2022-06-12 22:17:36 +0530

    create/insert

--- db_config.js
+++ db_config.js
@@ -1,18 +1,18 @@
-const mysql = require('mysql');
- var connection = mysql.createConnection({
-    host: "localhost",
-  user: "root",
-  password: "1234",
-  database: ""
- });
+// const mysql = require('mysql');
+//  var connection = mysql.createConnection({
+//     host: "localhost",
+//   user: "root",
+//   password: "1234",
+//   database: ""
+//  });
 
 
- connection.connect(function(err){
-    if(!!err){
-         console.log(err);
-    }
-    else{
-        console.log("connected");
-    }
- });
-  module.exports  = connection;
+//  connection.connect(function(err){
+//     if(!!err){
+//          console.log(err);
+//     }
+//     else{
+//         console.log("connected");
+//     }
+//  });
+//   module.exports  = connection;
