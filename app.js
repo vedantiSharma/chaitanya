76adddb371b79e9716da306eb40b8500554471e1
Vedanti Sharma <vedanti28sharma@gmail.com>
2022-06-13 22:05:36 +0530

    idcard

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var studentrouter = require('./routes/student');
 var loginRouter = require('./routes/login');
+ var idcardRouter = require('./routes/idcard');
 // var registerRouter = require('./routes/register');
 // var registerRouter = require('./routes/register');
 var  mysql = require('./db_config');
@@ -30,6 +31,7 @@ app.use('/', indexRouter)
 app.use('/student', studentrouter)
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
+app.use('/idcard', idcardRouter);
 // app.use('/register', registerRouter);
 
 
